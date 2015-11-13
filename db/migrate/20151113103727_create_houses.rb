74225653ff02f48eb927967f8bebcfd6822ff3a4
Chris DiMartino <vwchris@gmail.com>
2015-11-13 10:50:51 -0500

    Make House::name a not nullable value
    - As per PR commentary!

--- db/migrate/20151113103727_create_houses.rb
+++ db/migrate/20151113103727_create_houses.rb
@@ -2,7 +2,7 @@ class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
       t.integer :max_occupancy, null: false
-      t.string  :name
+      t.string  :name, null: false
     end
   end
 end
